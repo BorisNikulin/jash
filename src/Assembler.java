a9b58172cb1e967389ebb0c66b4216e34f31270a
Boris Nikulin <NikulinBE@gmail.com>
2017-03-16 18:33:46 -0700

    Assembler now uses pre-built code for interactive or command line use

--- src/Assembler.java
+++ src/Assembler.java
@@ -2,7 +2,6 @@
 //Author info here
 //TODO: don't forget to document each method in all classes!
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -43,8 +42,9 @@ public class Assembler
 			keyboard.close();
 		}
 
-		outputFileName = inputFileName.substring(0,
-				inputFileName.lastIndexOf('.')) + ".hack";
+		String[] pathSections = dissectPath(inputFileName);
+		
+		outputFileName = pathSections[0] + pathSections[1] + ".hack";
 
 		try
 		{
@@ -69,7 +69,6 @@ public class Assembler
 	// HINT: when should rom address increase? What kind of commands?
 	private static void firstPass(String inputFileName, SymbolTable symbolTable)
 	{
-		// "C:\\Users\\Boris\\Desktop\\nand2tetris\\projects\\06\\pong\\Pong.asm"
 		Parser parser = new Parser(inputFileName);
 		int curROM = 0;
 
@@ -161,19 +160,49 @@ public class Assembler
 
 		outputFile.close();
 	}
-	
-	public static String getFileName(String filePath)
-	{
-		int nameStartIndex = filePath.lastIndexOf('/');
-		if(filePath.lastIndexOf('/') >= 0)
-		{nameStartIndex + 1 }
-		else if( filePath.lastIndexOf('\\')
-		nameStartIndex = nameStartIndex >= 0
-				? nameStartIndex + 1 : 0;
 
-		int compEndIndex = filePath.indexOf(';');
-		compEndIndex = compEndIndex >= 0
-				? compEndIndex : filePath.length();
+	/**
+	 * Takes a file path as a string and returns an array with the first element
+	 * being the part of the string before the name, the second element with the
+	 * name, and the third part of the array with the extension. Where there is
+	 * no applicable part from the file path, the corresponding section will be
+	 * empty. For instance if there is no extension the the third element of the
+	 * returned array will have an empty string. Concatenating the array
+	 * together from the first to the last element will result in the original
+	 * file path.
+	 * 
+	 * @param filePath
+	 * @return
+	 */
+	public static String[] dissectPath(String filePath)
+	{
+		int nameStartIndex;
+		if ((nameStartIndex = filePath.lastIndexOf('/')) >= 0)
+		{
+			nameStartIndex += 1;
+		}
+		else if ((nameStartIndex = filePath.lastIndexOf('\\')) >= 0)
+		{
+			nameStartIndex += 1;
+		}
+		else
+		{
+			nameStartIndex = 0;
+		}
+
+		int nameEndIndex = filePath.indexOf('.');
+		if (nameEndIndex < 0)
+		{
+			nameEndIndex = filePath.length();
+		}
+
+		String[] dissectedPaths = new String[3];
+
+		dissectedPaths[0] = filePath.substring(0, nameStartIndex);
+		dissectedPaths[1] = filePath.substring(nameStartIndex, nameEndIndex);
+		dissectedPaths[2] = filePath.substring(nameEndIndex, filePath.length());
+
+		return dissectedPaths;
 	}
 
 }
